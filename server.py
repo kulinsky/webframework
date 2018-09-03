5d99ff007e55bb9c0e5c98b93608383fdbdfd19f
Anton Kulinsky <ankulinsky@gmail.com>
2018-09-03 12:52:27 +0500

    server

--- server.py
+++ server.py
@@ -1,9 +1,20 @@
 from wsgiref.simple_server import make_server
 
+from webframework.webframework import WebFramework, Response
 
-def main():
-    pass
+
+app = WebFramework()
+
+
+@app.route("^/$")
+def home(request):
+    return Response(body="Home".encode())
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        httpd = make_server('', 8000, app)
+        print('Serving on port 8000...')
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print('Goodbye.')
