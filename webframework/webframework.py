ebdab2be18053e4a4b906e6eedfb5dd1186b0c2e
Anton Kulinsky <ankulinsky@gmail.com>
2018-09-03 12:52:00 +0500

    Request, Response, WebFramework

---
+++ webframework/webframework.py
@@ -0,0 +1,84 @@
+import sys
+import re
+import traceback
+
+from urllib.parse import parse_qs
+
+
+class Request:
+    def __init__(self, environ):
+        self.environ = environ
+        self.path = environ["PATH_INFO"]
+        self.method = environ['REQUEST_METHOD']
+        self.headers = {k: v for k, v in environ.items() if k.startswith("HTTP_")}
+        self.params = None
+        self.GET = parse_qs(environ["QUERY_STRING"])
+        self.POST = {}
+
+        if self.method == "POST":
+            try:
+                content_length = int(environ.get("CONTENT_LENGTH", 0))
+            except ValueError:
+                content_length = 0
+
+            query_string = environ["wsgi.input"].read(content_length)
+            self.POST = str(query_string)
+
+
+class Response:
+    def __init__(self, status="200 OK", body=""):
+        self.status = status
+        self.body = body
+        self.headers = {
+            "Content-Type": "text/html",
+            "Content-Length": str(len(self.body))
+        }
+
+    @property
+    def wsgi_headers(self):
+        return [(k, v) for k, v in self.headers.items()]
+
+
+class WebFramework:
+    def __init__(self):
+        self.routes = {}
+
+    def __call__(self, environ, start_response):
+        response = self.dispatch(Request(environ))
+        start_response(response.status, response.wsgi_headers)
+        return [response.body]
+
+    def route(self, route_regex):
+        def route_decorator(fn):
+            self.routes[route_regex] = fn
+            return fn
+
+        return route_decorator
+
+    @staticmethod
+    def not_found():
+        return Response(
+            status="404 Not Found",
+            body=b"Page not found."
+        )
+
+    @staticmethod
+    def internal_error():
+        return Response(
+            status="500 Internal Server Error",
+            body=b"An error has occurred."
+        )
+
+    def dispatch(self, request):
+        for regex, view_func in self.routes.items():
+            match = re.search(regex, request.path)
+            if match is not None:
+                request.params = match.groups()
+                try:
+                    return view_func(request)
+                except Exception:
+                    exc_type, exc_value, exc_traceback = sys.exc_info()
+                    traceback.print_tb(exc_traceback)
+                    return self.internal_error()
+
+        return self.not_found()
