6478aeee3054ecb0689263500e54d0d7ff91acb6
Anton Kulinsky <ankulinsky@gmail.com>
2018-09-04 01:14:24 +0500

    SQLWorker now is abstract class

--- webframework/databases/sqlworker.py
+++ webframework/databases/sqlworker.py
@@ -1,23 +1,51 @@
-import sqlite3
+from contextlib import closing
+from abc import ABC, abstractmethod
 
 
-class SQLWorker:
+class SQLWorker(ABC):
+    @abstractmethod
     def create(self):
-        raise NotImplementedError
-    
+        raise NotImplementedError('users must define create')
+
+    @abstractmethod
     def read(self):
-        raise NotImplementedError
+        raise NotImplementedError('users must define read')
 
+    @abstractmethod
     def update(self):
-        raise NotImplementedError
+        raise NotImplementedError('users must define update')
 
+    @abstractmethod
     def delete(self):
-        raise NotImplementedError
+        raise NotImplementedError('users must define delete')
 
 
 class SQLiteWorker(SQLWorker):
     def __init__(self, filename='sqlite.db'):
+        import sqlite3
         self.conn = sqlite3.connect(filename)
+
+    def create(self, query, params={}):
+        try:
+            with closing(self.conn.cursor()) as cursor:
+                cursor.execute(query, params)
+                self.conn.commit()
+            return data
+        except sqlite3.IntegrityError:
+            print("couldn't insert ", *params.items())
+
+    def read(self, query):
+        with closing(self.conn.cursor()) as cursor:
+            cursor.execute(query)
+            data = cursor.fetchall()
+        return data
+
+    def update(self, query, params={}):
+        pass
+
+    def delete(self, query, params={}):
+        pass
+
 # try:
 #     with con:
 #         con.execute("insert into person(firstname) values (?)", ("Joe",))
