f7d58a9f1a1e0522ff58875394d8c6925b138cc1
Anton Kulinsky <ankulinsky@gmail.com>
2018-09-04 07:52:43 +0500

    add 'deleted' field

--- webframework/databases/fill_data.sql
+++ webframework/databases/fill_data.sql
@@ -7,34 +7,38 @@ CREATE TABLE "users" (
 	`phone`	TEXT,
 	`email`	TEXT,
 	`city_id`	INTEGER,
-	FOREIGN KEY(`city_id`) REFERENCES cities(id)
+	`deleted`	INTEGER DEFAULT 0,
+	FOREIGN KEY(`city_id`) REFERENCES `cities`(`id`)
 );
-CREATE TABLE `regions` (
+CREATE TABLE "regions" (
 	`id`	INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
-	`name`	TEXT NOT NULL
+	`name`	TEXT NOT NULL,
+	`deleted`	INTEGER DEFAULT 0
 );
-INSERT INTO `regions` (id,name) VALUES (1,'Краснодарский край'),
- (2,'Ростовская область'),
- (3,'Ставропольский край');
-CREATE TABLE `comments` (
+INSERT INTO `regions` (id,name,deleted) VALUES (1,'Краснодарский край',0),
+ (2,'Ростовская область',0),
+ (3,'Ставропольский край',0);
+CREATE TABLE "comments" (
 	`id`	INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
 	`text`	TEXT NOT NULL,
 	`user_id`	INTEGER,
-	FOREIGN KEY(`user_id`) REFERENCES users(id)
+	`deleted`	INTEGER DEFAULT 0,
+	FOREIGN KEY(`user_id`) REFERENCES `users`(`id`)
 );
-CREATE TABLE `cities` (
+CREATE TABLE "cities" (
 	`id`	INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
 	`name`	TEXT NOT NULL,
 	`region_id`	INTEGER,
-	FOREIGN KEY(`region_id`) REFERENCES regions(id)
+	`deleted`	INTEGER DEFAULT 0,
+	FOREIGN KEY(`region_id`) REFERENCES `regions`(`id`)
 );
-INSERT INTO `cities` (id,name,region_id) VALUES (1,'Краснодар',1),
- (2,'Кропоткин',1),
- (3,'Славянск',1),
- (4,'Ростов',2),
- (5,'Шахты',2),
- (6,'Батайск',2),
- (7,'Ставрополь',3),
- (8,'Пятигорск',3),
- (9,'Кисловодск',3);
+INSERT INTO `cities` (id,name,region_id,deleted) VALUES (1,'Краснодар',1,0),
+ (2,'Кропоткин',1,0),
+ (3,'Славянск',1,0),
+ (4,'Ростов',2,0),
+ (5,'Шахты',2,0),
+ (6,'Батайск',2,0),
+ (7,'Ставрополь',3,0),
+ (8,'Пятигорск',3,0),
+ (9,'Кисловодск',3,0);
 COMMIT;
