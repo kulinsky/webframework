95c9a2bc84ac07fcc0526a57d669e15ad4dbf940
Anton Kulinsky <ankulinsky@gmail.com>
2018-09-04 12:26:53 +0500

    add api get regions

--- server.py
+++ server.py
@@ -1,16 +1,40 @@
+import os
+import json
 from wsgiref.simple_server import make_server
 
 from webframework.webframework import WebFramework, Response
 
 
+TEMPLATE_DIR = '{}/templates/'.format(os.getcwd())
+
 app = WebFramework()
 
 
-@app.route("^/$")
+@app.route('^/$')
 def home(request):
-    data = app.db_read('select * from cities')
+    data = app.sql('select * from cities')
     print(data)
-    return Response(body="Home".encode())
+    return Response(body='Home'.encode())
+
+
+@app.route('^/comment/')
+def comment(request):
+    template = 'comment.html'
+
+    if request.method == 'POST':
+        print(request.POST)
+
+    with open('{}{}'.format(TEMPLATE_DIR, template)) as f:
+        html = f.read()
+    return Response(body=html.encode())
+
+
+@app.route('^/api/get/regions/')
+def get_regions(request):
+    query = 'SELECT id, name FROM regions WHERE deleted=0'
+    regions = app.sql(query)
+    body = json.dumps(dict(regions), ensure_ascii=False)
+    return Response(body=body.encode(), content_type='application/json')
 
 
 if __name__ == '__main__':
